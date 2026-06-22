a90d49d1fee7537e6faea3acd3756e66a5ea9147
codeshell <git@tmw.app>
2026-06-22 13:52:27 +0200

    chore: free up index for submodule

--- docs/_static/js/snippets.js
+++
@@ -1,8 +0,0 @@
-//apply code font on numbers for wiki table admonition
-
-const admonition = document.querySelectorAll(".admonition.wiki table td");
-admonition.forEach((item) => {
-	if (item.textContent.match(/^\d+$/)) {
-		item.style.fontFamily = "var(--md-code-font)";
-	}
-});
